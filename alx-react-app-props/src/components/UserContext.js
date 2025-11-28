44bf6dc87e5d83bd7a95a83cb3ec3df46ee9d233
Deyon Avoseh <avodeyon.07@gmail.com>
2025-11-28 19:15:00 +0100

    Modified alx-react-app-props

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -2,3 +2,9 @@ import { createContext } from 'react';
 
 // Create the context - this is like creating a "channel" for data
 export const UserContext = createContext();
+
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+export default UserContext; 
